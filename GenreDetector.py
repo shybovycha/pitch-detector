1dbdc3128b4dcac88eba0cde355fb40ecb1c3293
Artem Szubowicz <shybovycha@gmail.com>
2015-12-27 22:19:38 +0100

    Moved to convolutional network with nolearn and lasagne. It finally works! Yay! :star:

--- GenreDetector.py
+++ GenreDetector.py
@@ -2,209 +2,212 @@
 import numpy as np
 import pickle
 from os.path import isfile
-from pybrain.datasets import ClassificationDataSet
-from pybrain.datasets import SupervisedDataSet
-from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules.linearlayer import LinearLayer
-from pybrain.structure.modules.tanhlayer import TanhLayer
-from pybrain.structure.moduleslice import ModuleSlice
-from pybrain.structure.networks.feedforward import FeedForwardNetwork
-from pybrain.structure.connections.shared import MotherConnection, SharedFullConnection
-from pybrain.structure.modules.sigmoidlayer import SigmoidLayer
-
-
-class SimpleConvolutionalNetwork(FeedForwardNetwork):
-    """ A network with a specific form of weight-sharing, on a single 2D layer,
-    convoluting neighboring inputs (within a square). """
-
-    def __init__(self, inputdim, insize, convSize, numFeatureMaps, **args):
-        FeedForwardNetwork.__init__(self, **args)
-        inlayer = LinearLayer(inputdim * insize * insize)
-        self.addInputModule(inlayer)
-        self._buildStructure(inputdim, insize, inlayer, convSize, numFeatureMaps)
-        self.sortModules()
-
-    def _buildStructure(self, inputdim, insize, inlayer, convSize, numFeatureMaps):
-        # build layers
-        outdim = insize - convSize + 1
-        hlayer = TanhLayer(outdim * outdim * numFeatureMaps, name='h')
-        self.addModule(hlayer)
-
-        outlayer = SigmoidLayer(outdim * outdim, name='out')
-        self.addOutputModule(outlayer)
-
-        # build shared weights
-        convConns = []
-        for i in range(convSize):
-            convConns.append(MotherConnection(convSize * numFeatureMaps * inputdim, name='conv' + str(i)))
-        outConn = MotherConnection(numFeatureMaps)
-
-        # establish the connections.
-        for i in range(outdim):
-            for j in range(outdim):
-                offset = i * outdim + j
-                outmod = ModuleSlice(hlayer, inSliceFrom=offset * numFeatureMaps,
-                                     inSliceTo=(offset + 1) * numFeatureMaps,
-                                     outSliceFrom=offset * numFeatureMaps, outSliceTo=(offset + 1) * numFeatureMaps)
-                self.addConnection(
-                    SharedFullConnection(outConn, outmod, outlayer, outSliceFrom=offset, outSliceTo=offset + 1))
-
-                for k, mc in enumerate(convConns):
-                    offset = insize * (i + k) + j
-                    inmod = ModuleSlice(inlayer, outSliceFrom=offset * inputdim,
-                                        outSliceTo=offset * inputdim + convSize * inputdim)
-                    self.addConnection(SharedFullConnection(mc, inmod, outmod))
-
+import lasagne
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
 
 X = np.array([
     # crosses
-    [0, 1, 0, 0, 1,
-     0, 0, 1, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 1, 0,
+     0, 0, 1, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     1, 0, 0, 0, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 1, 0, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 1, 0, 1,
-     0, 0, 0, 1, 0,
-     0, 0, 1, 0, 1,
-     0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 0, 1, 0, 1,
-     0, 0, 0, 1, 0,
-     0, 0, 1, 0, 1,
-     0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0,
-     0, 0, 1, 0, 1,
-     0, 0, 0, 1, 0,
-     0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     0, 1, 0, 0, 0,
-     1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     0, 1, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     0, 1, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     0, 1, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [1, 0, 1, 0, 0,
-     0, 1, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0,
+     0, 1, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [1, 0, 0, 0, 1,
-     0, 1, 0, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     1, 0, 0, 0, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
     # rectangle
 
-    [1, 1, 1, 1, 1,
-     1, 0, 0, 0, 1,
-     1, 0, 0, 0, 1,
-     1, 0, 0, 0, 1,
-     1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0,
+     1, 0, 0, 0, 1, 0,
+     1, 0, 0, 0, 1, 0,
+     1, 0, 0, 0, 1, 0,
+     1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0],
+
+    [1, 1, 1, 1, 0, 0,
+     1, 0, 0, 1, 0, 0,
+     1, 0, 0, 1, 0, 0,
+     1, 1, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
+
+    [0, 1, 1, 1, 1, 0,
+     0, 1, 0, 0, 1, 0,
+     0, 1, 0, 0, 1, 0,
+     0, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
+
+    [0, 0, 0, 0, 0, 0,
+     0, 1, 1, 1, 1, 0,
+     0, 1, 0, 0, 1, 0,
+     0, 1, 0, 0, 1, 0,
+     0, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0],
+
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 1, 1, 1, 1,
+     0, 0, 1, 0, 0, 1,
+     0, 0, 1, 0, 0, 1,
+     0, 0, 1, 1, 1, 1],
 
     # triangles
 
-    [0, 0, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     1, 1, 1, 1, 1,
-     0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 1,
-     0, 0, 0, 1, 1,
-     0, 0, 1, 0, 1,
-     0, 1, 0, 0, 1,
-     1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0,
+     0, 0, 0, 1, 1, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 1, 0, 0, 1, 0,
+     1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [1, 0, 0, 0, 0,
-     1, 1, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     1, 0, 0, 1, 0,
-     1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0,
+     1, 1, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     1, 0, 0, 1, 0, 0,
+     1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [1, 1, 1, 1, 1,
-     0, 1, 0, 1, 0,
-     0, 0, 1, 0, 0,
-     0, 0, 0, 0, 0,
-     0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 0, 1, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [1, 0, 0, 0, 0,
-     1, 1, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     1, 1, 0, 0, 0,
-     1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0,
+     1, 1, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     1, 1, 0, 0, 0, 0,
+     1, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 1,
-     0, 0, 0, 1, 1,
-     0, 0, 1, 0, 1,
-     0, 0, 0, 1, 1,
-     0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 0,
+     0, 0, 0, 1, 1, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 0, 0, 1, 1, 0,
+     0, 0, 0, 0, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 1,
-     0, 0, 0, 1, 1,
-     0, 0, 1, 0, 1,
-     0, 1, 1, 1, 1,
-     0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0,
+     0, 0, 0, 1, 1, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 0, 0, 0, 1,
-     0, 0, 0, 1, 1,
-     0, 0, 1, 0, 1,
-     0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 1, 0,
+     0, 0, 0, 1, 1, 0,
+     0, 0, 1, 0, 1, 0,
+     0, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 0, 0, 1, 0,
-     0, 0, 1, 1, 0,
-     0, 1, 0, 1, 0,
-     1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0,
+     0, 0, 0, 1, 0, 0,
+     0, 0, 1, 1, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     1, 1, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     1, 0, 0, 0, 0,
-     1, 1, 0, 0, 0,
-     1, 0, 1, 0, 0,
-     1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0,
+     1, 0, 0, 0, 0, 0,
+     1, 1, 0, 0, 0, 0,
+     1, 0, 1, 0, 0, 0,
+     1, 1, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0],
 
-    [0, 0, 0, 0, 0,
-     0, 1, 0, 0, 0,
-     0, 1, 1, 0, 0,
-     0, 1, 0, 1, 0,
-     0, 1, 1, 1, 1]])
+    [0, 0, 0, 0, 0, 0,
+     0, 1, 0, 0, 0, 0,
+     0, 1, 1, 0, 0, 0,
+     0, 1, 0, 1, 0, 0,
+     0, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0]])
 
 Y = np.array([[1, 0, 0],  # cross (X)
               [1, 0, 0],  # cross (X)
@@ -220,6 +223,10 @@ Y = np.array([[1, 0, 0],  # cross (X)
               [1, 0, 0],  # cross (X)
 
               [0, 1, 0],  # square
+              [0, 1, 0],  # square
+              [0, 1, 0],  # square
+              [0, 1, 0],  # square
+              [0, 1, 0],  # square
 
               [0, 0, 1],  # triangle
               [0, 0, 1],  # triangle
@@ -233,45 +240,79 @@ Y = np.array([[1, 0, 0],  # cross (X)
               [0, 0, 1],  # triangle
               [0, 0, 1]])  # triangle
 
-Y1 = np.array([[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [1], [2], [2], [2], [2], [2], [2], [2], [2], [2], [2], [2]])
+# 3 - cross, 2 - circle, 1 - triangle
+Y1 = np.array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
-saved_network_filename = 'network01.pickle'
+saved_network_filename = 'network02.pickle'
 
 if isfile(saved_network_filename):
-    print "Loading network..."
+    print("Loading network...")
 
-    net = pickle.load(open(saved_network_filename, 'r'))
+    net = pickle.load(open(saved_network_filename, 'rb'))
 else:
-    print "Building network..."
+    print("Building network...")
 
-    net = SimpleConvolutionalNetwork(1, 5, 2, 2)
+    net1 = NeuralNet(
+        layers=[('input', layers.InputLayer),
+                ('conv2d1', layers.Conv2DLayer),
+                ('maxpool1', layers.MaxPool2DLayer),
+                ('conv2d2', layers.Conv2DLayer),
+                ('maxpool2', layers.MaxPool2DLayer),
+                ('dropout1', layers.DropoutLayer),
+                ('dense', layers.DenseLayer),
+                ('dropout2', layers.DropoutLayer),
+                ('output', layers.DenseLayer),
+                ],
+        # input layer
+        input_shape=(None, 1, 6, 6),
+        # layer conv2d1
+        conv2d1_num_filters=25,
+        conv2d1_filter_size=(3, 3),
+        conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
+        conv2d1_W=lasagne.init.GlorotUniform(),
+        # layer maxpool1
+        maxpool1_pool_size=(1, 1),
+        # layer conv2d2
+        conv2d2_num_filters=25,
+        conv2d2_filter_size=(3, 3),
+        conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
+        # layer maxpool2
+        maxpool2_pool_size=(2, 2),
+        # dropout1
+        dropout1_p=0.5,
+        # dense
+        dense_num_units=25,
+        dense_nonlinearity=lasagne.nonlinearities.rectify,
+        # dropout2
+        dropout2_p=0.5,
+        # output
+        output_nonlinearity=lasagne.nonlinearities.softmax,
+        output_num_units=10,
+        # optimization method params
+        update=nesterov_momentum,
+        update_learning_rate=0.1,
+        update_momentum=0.9,
+        max_epochs=500,
+        verbose=0,
+    )
 
-    # 2 = dimensionality of the each input vector; 1 = number of output types; nb_classes = number of classes
-    # ds = ClassificationDataSet(, 1, nb_classes=2)
+    print("Training network...")
 
-    ds = SupervisedDataSet(25, 16)
+    net = net1.fit(X.reshape((-1, 1, 6, 6)), Y1.astype(np.uint8))
 
-    for i in range(len(Y)):
-        ds.addSample(X[i], np.append(Y[i], np.zeros(16 - len(Y[i]))))
+    print("Saving network...")
 
-    trainer = BackpropTrainer(net, ds)
-
-    print "Training network..."
-
-    trainer.trainEpochs(1000)
-
-    print "Saving network..."
-
-    f = open(saved_network_filename, 'w')
+    f = open(saved_network_filename, 'wb')
     pickle.dump(net, f)
     f.close()
 
-print "Testing:"
+print("Testing:")
 
-X1 = np.array([[0, 0, 0, 1, 0,
-                0, 0, 1, 1, 0,
-                0, 1, 0, 1, 0,
-                1, 1, 1, 1, 0,
-                0, 0, 0, 0, 0]])
+X1 = np.array([[0, 1, 0, 0, 0, 0,
+                0, 0, 1, 0, 1, 0,
+                0, 0, 0, 1, 0, 0,
+                0, 0, 1, 0, 1, 0,
+                0, 1, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0]])
 
-print net.activate(X1[0])
+print(net.predict(X1.reshape((-1, 1, 6, 6))))
