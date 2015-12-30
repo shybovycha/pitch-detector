930624fc073eec08c946aecc4c8bf7811e43c1ca
Artem Szubowicz <shybovycha@gmail.com>
2015-12-30 01:35:17 +0100

    Moving forward to Fourier transform

--- GenreDetector.py
+++ GenreDetector.py
@@ -3,100 +3,116 @@ import numpy as np
 import pickle
 from os.path import isfile
 import lasagne
+import theano
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 import wave
 import glob
-from joblib import Parallel, delayed
-import multiprocessing
 
 
-def running_mean(x, n):
-    cum_sum = np.cumsum(np.insert(x, 0, 0))
-    return (cum_sum[n:] - cum_sum[:-n]) / n
+class GenreDetector:
+    def __init__(self):
+        self.X, self.Y = [], []
+        self.min_length, self.max_length = 0, 0
+        self.genres = ['metal', 'dubstep', 'acoustic']
 
+        self.dataset_x_filename = 'learn_dataset03_x.pickle'
+        self.dataset_y_filename = 'learn_dataset03_y.pickle'
+        self.network_filename = 'network03.pickle'
 
-def get_wave_data(file_path):
-    with wave.open(file_path, 'rb') as wf:
-        sample_width = wf.getsampwidth()
-        frame_rate = wf.getframerate()
-        num_frames = wf.getnframes()
+        self.net = None
 
-        length_in_seconds = num_frames / (frame_rate * 1.0)
+    def run(self):
+        self.get_network()
 
-        num_iterations = int(length_in_seconds * 2)
-        iteration_size = int(num_frames / num_iterations)
+    def get_wave_data(self, file_path):
+        with wave.open(file_path, 'rb') as wf:
+            sample_width = wf.getsampwidth()
+            frame_rate = wf.getframerate()
+            num_frames = wf.getnframes()
 
-        wave_data = []
+            length_in_seconds = num_frames / (frame_rate * 1.0)
 
-        for fragment_num in range(0, num_iterations):
-            data = wf.readframes(iteration_size)
+            num_iterations = int(length_in_seconds * 2)
+            iteration_size = int(num_frames / num_iterations)
 
-            # window = np.blackman(len(data) / sample_width)
-            # in_data = np.array(wave.struct.unpack("%dh" % (len(data) / sample_width), data)) * window
-            # wave_data = np.hstack((wave_data, in_data))
+            wave_data = []
 
-            fragment_data = wave.struct.unpack("%dh" % (len(data) / sample_width), data)
+            for fragment_num in range(0, num_iterations):
+                data = wf.readframes(iteration_size)
 
-            wave_data = np.hstack((wave_data, running_mean(fragment_data, n=len(fragment_data) / 2)))
+                window_size = len(data) / sample_width
+                window = np.blackman(window_size)
 
-    return wave_data
+                fragment_data = wave.struct.unpack("%dh" % window_size, data) * window
+                fft_data = abs(np.fft.rfft(fragment_data)) ** 2
 
+                wave_data = np.hstack((wave_data, fft_data[:100]))  # take only 100 frequences
 
-def dump_to_file(filename, arg):
-    with open(filename, 'wb') as df:
-        pickle.dump(arg, df)
+        return wave_data
 
+    def dump_to_file(self, filename, arg):
+        with open(filename, 'wb') as df:
+            pickle.dump(arg, df)
 
-def load_from_file(filename):
-    return pickle.load(open(filename, 'rb'))
+    def load_from_file(self, filename):
+        return pickle.load(open(filename, 'rb'))
 
+    def find_songs(self):
+        self.files = []
 
-def find_songs():
-    files = []
+        for genre_idx, genre in enumerate(self.genres):
+            genre_path = 'songs/{0}/*.wav'.format(genre)
+            self.files += [[f, genre_idx] for f in glob.iglob(genre_path)]
 
-    for genre in ['metal', 'dubstep', 'acoustic']:
-        genre_path = 'songs/{0}/*.wav'.format(genre)
-        files += [[f, genre] for f in glob.iglob(genre_path)]
+    def get_song_data(self, pair):
+        filename, genre_idx = pair
 
-    return files
+        song_data = self.get_wave_data(filename)
 
+        self.X.append(song_data)
+        self.Y.append(genre_idx)
 
-if __name__ == '__main__':
-    X, Y = [], []
+    def load_training_data(self):
+        self.X = self.load_from_file(self.dataset_x_filename)
+        self.Y = self.load_from_file(self.dataset_y_filename)
 
+        if isinstance(self.Y[0], str):
+            self.Y = [self.genres.index(y) for y in self.Y]
 
-    def get_song_data(pair):
-        filename, genre = pair
+        self.X = np.array(self.X)
+        self.Y = np.array(self.Y)
 
-        song_data = get_wave_data(filename)
+        self.normalize_dataset()
 
-        X.append(song_data)
-        Y.append(genre)
+    def normalize_dataset(self):
+        lengths = [len(d) for d in self.X]
+        self.min_length, self.max_length = min(lengths), max(lengths)
 
+        # fill smaller data chunks with zeros
+        self.X = np.array([np.hstack((x, np.zeros(self.max_length - len(x)))) for x in self.X])
+        self.X = self.X.reshape((-1, 1, self.max_length))
+        self.X = (self.X * 1000).astype(np.uint64)
 
-    num_cores = multiprocessing.cpu_count()
-    input_files = find_songs()
+        self.Y = np.array(self.Y)
 
-    for i, pair in enumerate(input_files):
-        print('>> Loading file {0} ({1} / {2})'.format(pair[0], i, len(input_files)))
-        get_song_data(pair)
+    def create_training_data(self):
+        input_files = self.find_songs()
 
-    lengths = [len(d) for d in X]
-    min_length, max_length = min(lengths), max(lengths)
+        for i, pair in enumerate(input_files):
+            print('>> Loading file {0} ({1} / {2})'.format(pair[0], i, len(input_files)))
+            self.get_song_data(pair)
 
-    print('min length:', min_length, 'max length:', max_length)
+        self.normalize_dataset()
 
-    saved_network_filename = 'network03.pickle'
+        self.dump_to_file(self.dataset_x_filename, self.X)
+        self.dump_to_file(self.dataset_y_filename, self.Y)
 
-    if isfile(saved_network_filename):
-        print("Loading network...")
+    def load_network(self):
+        self.net = self.load_from_file(self.network_filename)
 
-        net = pickle.load(open(saved_network_filename, 'rb'))
-    else:
-        print("Building network...")
-
+    def train_network(self):
         net1 = NeuralNet(
             layers=[('input', layers.InputLayer),
                     ('conv1d1', layers.Conv1DLayer),
@@ -109,20 +125,20 @@ if __name__ == '__main__':
                     ('output', layers.DenseLayer),
                     ],
             # input layer
-            input_shape=(None, 1, 1, max_length),
+            input_shape=(None, 1, self.max_length),
             # layer conv2d1
             conv1d1_num_filters=25,
-            conv1d1_filter_size=(1, 4000),
+            conv1d1_filter_size=10,
             conv1d1_nonlinearity=lasagne.nonlinearities.rectify,
             conv1d1_W=lasagne.init.GlorotUniform(),
             # layer maxpool1
-            maxpool1_pool_size=(1, 4),
+            maxpool1_pool_size=25,
             # layer conv2d2
             conv1d2_num_filters=25,
-            conv1d2_filter_size=(1, 4000),
+            conv1d2_filter_size=100,
             conv1d2_nonlinearity=lasagne.nonlinearities.rectify,
             # layer maxpool2
-            maxpool2_pool_size=(2, 8),
+            maxpool2_pool_size=25,
             # dropout1
             dropout1_p=0.5,
             # dense
@@ -132,21 +148,37 @@ if __name__ == '__main__':
             dropout2_p=0.5,
             # output
             output_nonlinearity=lasagne.nonlinearities.softmax,
-            output_num_units=10,
+            output_num_units=len(self.genres),
             # optimization method params
             update=nesterov_momentum,
             update_learning_rate=0.1,
             update_momentum=0.9,
             max_epochs=500,
-            verbose=0,
+            verbose=1,
         )
 
         print("Training network...")
 
-        net = net1.fit(X.reshape((-1, 1, 1, max_length)), Y.astype(np.uint8))
+        self.net = net1.fit(self.X, self.Y.astype(np.uint8))
 
         print("Saving network...")
 
-        f = open(saved_network_filename, 'wb')
-        pickle.dump(net, f)
-        f.close()
+        self.dump_to_file(self.network_filename, self.net)
+
+    def get_training_data(self):
+        if isfile(self.dataset_x_filename):
+            self.load_training_data()
+        else:
+            self.create_training_data()
+
+    def get_network(self):
+        if isfile(self.network_filename):
+            self.load_network()
+        else:
+            self.get_training_data()
+            self.train_network()
+
+
+if __name__ == '__main__':
+    detector = GenreDetector()
+    detector.run()
